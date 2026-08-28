3e7b920b127fef6e6ec45007b6a95a4eaf80a24b
Maaz Ali <maazalisshahid@gmail.com>
2026-08-28 23:56:40 +0500

    fix(csp): add ws: and wss: to connect-src in Content Security Policy

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value:
-      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; frame-src 'self' https://js.stripe.com https://hooks.stripe.com; img-src 'self' data: blob: https:; media-src 'self' blob: https: https://*.public.blob.vercel-storage.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://api.stripe.com https://*.public.blob.vercel-storage.com;",
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; frame-src 'self' https://js.stripe.com https://hooks.stripe.com; img-src 'self' data: blob: https:; media-src 'self' blob: https: https://*.public.blob.vercel-storage.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' ws: wss: https://api.stripe.com https://*.public.blob.vercel-storage.com;",
   },
 ];
 
